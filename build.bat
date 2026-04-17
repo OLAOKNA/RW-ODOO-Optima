9924a45622f91955304938aabe5c495d738eaa7d
jrogowski <jatr@poczta.fm>
2026-04-17 09:48:45 +0200

    2026.04.17

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-.venv\Scripts\pyinstaller.exe --onefile --windowed --name "RW-ODOO-Optima" main.py
+.venv\Scripts\pyinstaller.exe --onedir --windowed --name "RW-ODOO-Optima" main.py
 echo.
 echo Plik exe znajduje sie w folderze dist\
 pause
